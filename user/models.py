e99d8b338c391e7968aefdf60d233e19bcb1b36e
catdingding <tigerrayson@gmail.com>
2020-05-21 16:35:51 +0800

    add user model

--- user/models.py
+++ user/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
 
-# Create your models here.
+
+class User(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=20, unique=True)
+    is_gm = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
