536cdde67456b000d60ebdf894e9d33a9da2ac89
catdingding <tigerrayson@gmail.com>
2021-02-05 17:22:56 +0800

    update chara record field

--- chara/models.py
+++ chara/models.py
@@ -112,6 +112,9 @@ class Chara(BaseModel):
 
         CharaAttribute.objects.bulk_update(attrs.values(), fields=['value'])
 
+        self.record.monthly_level_up += n_level
+        self.record.save()
+
     def get_items(self, kind, *args, **kwargs):
         assert kind in ['bag', 'storage']
         field = getattr(self, kind + '_items')
@@ -185,6 +188,8 @@ class CharaRecord(BaseModel):
 
     total_battle = models.IntegerField(default=0)
     monthly_level_up = models.IntegerField(default=0)
+
+    level_down_count = models.IntegerField(default=0)
 
 
 class CharaIntroduction(BaseModel):
