4380c6337d33456f6b010d8a4ea35f2da1714f3c
catdingding <tigerrayson@gmail.com>
2021-02-15 02:59:42 +0800

    update asgi

--- hero/asgi.py
+++ hero/asgi.py
@@ -4,14 +4,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from django.urls import path
 
-from .channels_middleware import JWTAuthMiddleware
+asgi_app = get_asgi_application()
 
+from .channels_middleware import JWTAuthMiddleware
 from system.consumers import ChatConsumer
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hero.settings')
 
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": asgi_app,
     "websocket": JWTAuthMiddleware(
         URLRouter(
             [path('ws/chat/', ChatConsumer.as_asgi())]
