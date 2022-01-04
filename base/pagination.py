36ab96d07b964476f641b55c24dea3d6bed8067b
catdingding <tigerrayson@gmail.com>
2022-01-04 20:53:37 +0800

    pagination page_number check

--- base/pagination.py
+++ base/pagination.py
@@ -1,3 +1,4 @@
+import math
 from rest_framework.pagination import PageNumberPagination
 
 
@@ -5,3 +6,23 @@ class BasePagination(PageNumberPagination):
     page_size = 100
     page_size_query_param = 'page_size'
     max_page_size = 100
+
+    def paginate_queryset(self, queryset, request, view=None):
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = self.get_page_number(request, paginator)
+        last_page = max(math.ceil(queryset.count() / page_size), 1)
+        if int(page_number) > last_page:
+            page_number = last_page
+
+        self.page = paginator.page(page_number)
+
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
+
+        self.request = request
+        return list(self.page)
